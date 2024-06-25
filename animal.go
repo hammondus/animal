c646a601ea4702d71f141c9d3b49d80e07cacd34
Craig Hammond <62829736+hammondus@users.noreply.github.com>
2024-06-25 10:06:13 +1000

    Update animal.go
    added some carriage returns

--- animal.go
+++ animal.go
@@ -3,9 +3,9 @@ package animal
 import "fmt"
 
 func Move(meters int) {
-	fmt.Printf("Moved %d meters", meters)
+	fmt.Printf("Moved %d meters\n", meters)
 }
 
 func Eat(kg int) {
-	fmt.Printf("Spent %d minutes eating", kg*5)
+	fmt.Printf("Spent %d minutes eating\n", kg*5)
 }
